c87ebf0140972acf60f2b02cdbb6aea1466794e8
Jordon Terry <jt5493@hotmail.com>
2020-01-30 04:01:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,14 +1,14 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
-  def batch_badge_creator(array)
-    list = []
-    array.each do |names|
-      list << badge_maker(name)
-    end
-    return list
-  end
 end
 
+def batch_badge_creator(array)
+  list = []
+  array.each do |names|
+    list << badge_maker(name)
+  end
+  return list
+end
 
 def assign_rooms(list)
 
