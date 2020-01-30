0b17da26dda45fdcb00c4ec25bfeda20b4476ec8
Jordon Terry <jt5493@hotmail.com>
2020-01-30 04:29:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,6 +10,8 @@ def batch_badge_creator(array)
   return list
 end
 
-def assign_rooms(list)
-
+def assign_rooms(speaker)
+  room.collect do |names|
+    room.each_with_index {|index| return "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"}
+  end
 end
